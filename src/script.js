8e631639de39d94358d56450489e1a88caec431e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-05-01 19:33:04 +0530

    Added the placeholders for cards

--- src/script.js
+++ src/script.js
@@ -4,6 +4,18 @@ document.querySelector("#deck").addEventListener("click",()=>{fetch("https://www
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    deckId = data.deck_Id
+    deckId = data.deck_id
+    console.log(deckId)
+
 })})
+
+
+document.querySelector(".draw").addEventListener("click",()=>{
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+
+})
 
