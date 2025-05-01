7bb166fe3994bcfdd4d95717dbab50f7a1e935bd
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-05-01 19:08:20 +0530

    Added New Deck Button

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 let deckId
 
-fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+document.querySelector("#deck").addEventListener("click",()=>{fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 .then(res => res.json())
 .then(data => {
+    console.log(data)
     deckId = data.deck_Id
-})
+})})
 
